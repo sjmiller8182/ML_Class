eae9ef151d5192e0781e83d94dcebc22c9bd8b7e
Stuart Miller <stuart.j.miller.8182@gmail.com>
2018-09-09 14:50:07 -0500

    initial commit of Temporal Difference files

--- Reinforcement Learning/plot_utils.py
+++ Reinforcement Learning/plot_utils.py
@@ -2,6 +2,21 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+import seaborn as sns
+sns.set_style("white")
+
+def plot_values_td(V):
+	# reshape the state-value function
+	V = np.reshape(V, (4,12))
+	# plot the state-value function
+	fig = plt.figure(figsize=(15,5))
+	ax = fig.add_subplot(111)
+	im = ax.imshow(V, cmap='cool')
+	for (j,i),label in np.ndenumerate(V):
+	    ax.text(i, j, np.round(label,3), ha='center', va='center', fontsize=14)
+	plt.tick_params(bottom='off', left='off', labelbottom='off', labelleft='off')
+	plt.title('State-Value Function')
+	plt.show()
 
 def plot_values(V):
 	# reshape value function
